80c1baac1e8b1a5a4e71cc5eac4ff1fa5f746411
Jerilyn Gonçalves <jerilyn.goncalves@gigigo.com>
2018-06-07 12:33:25 +0200

    Update: Add public methods for library [WIP]

--- GIGTranslations/GIGTranslations.swift
+++ GIGTranslations/GIGTranslations.swift
@@ -8,6 +8,45 @@
 
 import Foundation
 
+/// TODO - Add SDK's public class description
+///
+/// ### Usage
+/// TODO - Add usage description
+///
+/// ### Overview
+/// TODO - Add overview description
+///
+/// - Version: 1.0
+/// - Copyright: Gigigo S.L.
 open class GIGTranslations: NSObject {
     
+    // MARK: - Public methods
+    
+    /// Add description
+    ///
+    /// - Parameters:
+    ///   - configurationURL: Add description
+    ///   - bundle: Add description
+    public func setup(configurationURL: URL, bundle: Bundle?, completion: ((Bool) -> Void)?) {
+        // !!!
+    }
+    
+    
+    /// Add description
+    ///
+    /// - Parameters:
+    ///   - language: Add description
+    ///   - completion: Add description
+    public func set(language: String, completion: ((Bool) -> Void)?) {
+        // !!!
+    }
+    
+    /// Add description
+    ///
+    /// - Parameter key: Add description
+    /// - Returns: Add description
+    public func value(for key: String) -> String? {
+        // !!!
+        return nil
+    }
 }
